60ac2ed3e0fd7514a07c98524d8d4bc2762290cc
hamasyou <hamasyou@gmail.com>
2015-11-05 10:07:26 +0900

    3と5で割り切れるときに FizzBuzz と表示するように実装

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,7 +1,9 @@
 <?php
 
   for ($i = 1; $i <= 100; $i++) {
-    if ($i % 3 == 0) {
+    if ( ($i % 3 == 0) && ($i % 5 == 0) ) {
+      echo 'FizzBuzz';
+    } else if ($i % 3 == 0) {
       echo 'Fizz';
     } else if ($i % 5 == 0) {
       echo 'Buzz';
